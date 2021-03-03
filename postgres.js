0bb0548f989b7d8880895252479caeb4597b6a17
Evan Epperson <evanepperson@pop-os.localdomain>
2021-03-03 17:27:51 -0600

    saving

--- postgres.js
+++ postgres.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Client = require("pg").Client;
 // comment this out this is linux section 
 const client = new Client({
@@ -11,10 +10,6 @@ const client = new Client({
 // testing this out 
 
 // this is mac section 
-=======
-const Client = require("pg").Pool;
-// comment this out this is linux section
->>>>>>> dc4271845da9014e9d052b3466e3e754f63ca3ec
 // const client = new Client({
 // 	user: "evanepperson",
 // 	host: "localhost",
