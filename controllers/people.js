44f5d710ec38fb790b8f2d3c153e2dc109d1f006
Evan Epperson <evanepperson@pop-os.localdomain>
2021-03-03 15:56:14 -0600

    saving

--- controllers/people.js
+++ controllers/people.js
@@ -5,6 +5,7 @@ const postgres = require("../postgres.js");
 router.get("/", (req, res) => {
   postgres.query("SELECT * FROM people ORDER BY id ASC;", (err, results) => {
     console.log(results);
+    // testing again 
     res.json(results.rows);
   });
 });
