ae09d09ec1f5775b97a180b783faa5a8b8512e90
Evan Epperson <evanepperson@pop-os.localdomain>
2021-03-03 13:02:34 -0600

    basic crud filled out , changing things now

---
+++ postgres.js
@@ -0,0 +1,20 @@
+const Client = require("pg").Pool;
+// comment this out this is linux section 
+// const client = new Client({
+//   user: "evanepperson",
+//   host: "localhost",
+//   database: "contacts",
+//   password: "password",
+//   port: 5432,
+// });
+
+// this is mac section 
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || "postgresql://localhost:5432/contacts",
+});
+
+
+
+
+module.exports = client;
