a4d3f536428295460c35f571444f8971e1848fb8
Evan Epperson <evanepperson@pop-os.localdomain>
2021-03-03 19:18:06 -0600

    saving

--- postgres.js
+++ postgres.js
@@ -3,9 +3,10 @@ const Client = require("pg").Client;
 const client = new Client({
   user: "ptcucmxkslupdt",
   host: "localhost",
-  database: process.env.DATABASE_URL || "contacts",
+  database: process.env.DATABASE_URL || "contacts" ,
   password: "2b0d312d632a8fb55b658a4bcfcf0416684c96027b71dd6a427f5afb30534e53",
   port: 5432,
+  SETPGSSLMODE: require
   // ssl: true,
 });
 // testing this out right now to see if it can get rid of something 
