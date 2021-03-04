f5cc994a3d5e4534be7a72c5a49a1a86b8c4bd91
Evan Epperson <evanepperson@pop-os.localdomain>
2021-03-03 18:29:56 -0600

    saving

--- controllers/people.js
+++ controllers/people.js
@@ -10,47 +10,6 @@ router.get("/", (req, res) => {
 	});
 });
 
-router.post("/", (req, res) => {
-	postgres.query(
-		`INSERT INTO people (name, age) VALUES ('${req.body.name}', ${req.body.age})`,
-		(err, results) => {
-			postgres.query(
-				"SELECT * FROM people ORDER BY id ASC;",
-				(err, results) => {
-					res.json(results.rows);
-				}
-			);
-		}
-	);
-});
 
-router.delete("/:id", (req, res) => {
-	postgres.query(
-		`DELETE FROM people WHERE id = ${req.params.id};`,
-		(err, results) => {
-			postgres.query(
-				"SELECT * FROM people ORDER BY id ASC;",
-				(err, results) => {
-					res.json(results.rows);
-				}
-			);
-		}
-	);
-});
-// testing this out
-
-router.put("/:id", (req, res) => {
-	postgres.query(
-		`UPDATE people SET name = '${req.body.name}', AGE = ${req.body.age} WHERE id = ${req.params.id}`,
-		(err, results) => {
-			postgres.query(
-				"SELECT * FROM people ORDER BY id ASC;",
-				(err, results) => {
-					res.json(results.rows);
-				}
-			);
-		}
-	);
-});
 
 module.exports = router;
--- postgres.js
+++ postgres.js
@@ -1,29 +1,20 @@
 const Client = require("pg").Client;
 // comment this out this is linux section 
-// const client = new Client({
-//   user: "evanepperson",
-//   host: "localhost",
-//   database: "contacts",
-//   password: "password",
-//   port: 5432,
-// });
+const client = new Client({
+  user: "evanepperson",
+  host: "localhost",
+  database: "contacts",
+  password: "password",
+  port: 5432,
+});
 // testing this out 
 
-// this is mac section 
-// const client = new Client({
-// 	user: "evanepperson",
-// 	host: "localhost",
-// 	database: "contacts",
-// 	password: "password",
-// 	port: 5432,
-// });
-// testing this out
 
 // this is mac section
-const client = new Client({
-	connectionString:
-		process.env.DATABASE_URL || "postgresql://localhost:5432/contacts"
-});
+// const client = new Client({
+// 	connectionString:
+// 		process.env.DATABASE_URL || "postgresql://localhost:5432/contacts",
+// });
 // testing 
 
 module.exports = client;
