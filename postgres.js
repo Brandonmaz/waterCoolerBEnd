42d69f9c784941341fddf2c371e2def5b1dcf6f5
Evan Epperson <evanepperson@pop-os.localdomain>
2021-03-03 18:37:56 -0600

    saving

--- postgres.js
+++ postgres.js
@@ -1,20 +1,21 @@
 const Client = require("pg").Client;
 // comment this out this is linux section 
-// const client = new Client({
-//   user: "evanepperson",
-//   host: "localhost",
-//   database: "contacts",
-//   password: "password",
-//   port: 5432,
-// });
+const client = new Client({
+  user: "evanepperson",
+  host: "localhost",
+  database: process.env.DATABASE_URL,
+  password: "password",
+  port: 5432,
+  ssl: true
+});
 // testing this out 
 
 
 // this is mac section
-const client = new Client({
-	connectionString:
-		process.env.DATABASE_URL || "postgresql://localhost:5432/contacts",
-});
+// const client = new Client({
+// 	connectionString:
+// 		process.env.DATABASE_URL || "postgresql://localhost:5432/contacts",
+// });
 // testing 
 
 module.exports = client;
