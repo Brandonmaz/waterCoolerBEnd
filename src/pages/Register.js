16861d21f04ea02ab1badd1a7f3de1c42ceaaf29
banu tryon <banutryon@banus-mbp.lan>
2021-03-04 21:06:38 -0800

    finished todo

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { CredentialsContext } from "../App";
+import { handleErrors } from "./Login";
 
 export default function Register() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+	const [, setCredentials] = useContext(CredentialsContext);
+
 	const register = (e) => {
 		e.preventDefault();
 		fetch(`http://localhost:4000/register`, {
@@ -14,11 +20,26 @@ export default function Register() {
 				username,
 				password,
 			}),
-		});
+		})
+			.then(handleErrors)
+			.then(() => {
+				setCredentials({
+					username,
+					password,
+				});
+				history.push("/");
+			})
+			.catch((error) => {
+				setError(error.message);
+			});
 	};
+
+	const history = useHistory();
+
 	return (
 		<div>
 			<h1>Register</h1>
+			{error && <span style={{ color: "red" }}>{error}</span>}
 			<form onSubmit={register}>
 				<input
 					onChange={(e) => setUsername(e.target.value)}
